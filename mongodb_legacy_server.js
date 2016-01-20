3021969482bf6b3ab046994e8377f7e013420d5c
Jeremy Hoogstraten <jeremyhoogstraten@gmail.com>
2016-01-20 17:20:39 +0000

    Server.js with MongoDB integration which I won't be using on Heroku

---
+++ mongodb_legacy_server.js
@@ -0,0 +1,95 @@
+// Due to Heroku only allowing MongoDB aaS - which requires CC information, to charge in case you go over the bandwidth, I have opted to not use MongoDB.
+// Hence this "legacy" file is for when you'd like to use MongoDB.
+//
+// So why not simply fill in CC information? aaS has burned me before when a spambot got at my program, I owed 1500 dollar at the end of the month.
+// So no thank you, to that.
+//
+// Using MongoDB 1.4.4 due to dependency errors with Mongoskin through Monk when using Heroku
+var http = require('http');
+var URL = require('url');
+var monk = require('monk');
+var db = monk('localhost:27017/shortlinks');
+var collection = db.get("shorts");
+var fs = require("fs");
+
+var server = http.createServer(function(req, res) {
+  var path = URL.parse(req.url).pathname.toLowerCase();
+  
+  if(path.substr(0, 5) === "/new/") {
+    var newPath = path.substr(5);
+    if(newPath === "" ||  !isValidUrl(newPath)) return res.end("Please Enter a valid url.");
+      
+    isInDB(newPath, function(inDB) {
+      if(inDB) { // If it already exists, spew out the existing one, don't waste resources creating a new entry
+          
+        getShorts({ "original_url": newPath }, function(json) {
+          res.end(json);
+        });
+          
+      }
+      else insertIntoDB(newPath, function(json) {
+        res.end(json);
+      });
+    });
+  }
+  else if(/^\/[0-9]+$/.test(path)) {
+    getShorts({ "short_url": "https://evening-everglades-43718.herokuapp.com/"+path.substr(1) }, function(json) {
+      if(json === undefined) {
+        res.end("The requested shortlink does not exist");
+      } 
+      else res.end(json);
+    });
+  } else {
+    var r = fs.readFileSync("./public/index.html");
+		res.writeHead(200, "Content-Type", "text/html");
+		res.end(r);
+  }
+});
+
+function isValidUrl(path) {
+  return /^https?:\/\/(www.)?[A-Z0-9]+\..+/i.test(path);
+}
+
+function isInDB(path, callback) {
+  collection.find({ "original_url": path }, function(err, documents) {
+    if(err) throw err;
+    
+    var inDB = false;
+    
+    if(documents.length > 0) inDB = true;
+    else inDB = false;
+    
+    callback(inDB);
+  });
+}
+
+function getShorts(searchObj, callback) {
+  collection.find(searchObj, { _id: false, "original_url": true, "short_url": true }, function(err, documents) {
+    if(err) throw err;
+    
+    if(documents.length > 0) delete documents[0]["_id"]; // Since I, for some inexplicable reason, cannot remove the _id by using projection
+    var json = JSON.stringify(documents[0]);
+      
+    callback(json);
+  });
+}
+
+function insertIntoDB(path, callback) {
+  collection.count({}, function(err, docs) {
+    if(err) throw err;
+    
+    var num = docs+1;
+  
+    var json = { "original_url": path,
+                 "short_url": "https://evening-everglades-43718.herokuapp.com/"+num };
+    
+    collection.insert(json, function(err, data) {
+      if(err) throw err;
+      
+      delete json["_id"];
+      callback(JSON.stringify(json));
+    });
+  });
+}
+  
+server.listen(process.env.PORT || 8080);
